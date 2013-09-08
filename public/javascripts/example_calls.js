0659cfd75277a0bdbf7bbaf2658738240639d3c6
Brian Tomlinson <brian@smartm.no>
2013-09-08 17:04:22 +0200

    Expanded example to include PUT, DELETE, and an example of why you can't blindly throw around asynch calls and expect things to happen in a particular order.

--- public/javascripts/example_calls.js
+++ public/javascripts/example_calls.js
@@ -11,5 +11,29 @@ $(document).ready(function() {
             $('#aDiv').append('<p> findById gave us id: ' + data._id + '</p>');
             $('#aDiv').append('<p>'+ JSON.stringify(data, undefined, 2) + '</p>');
         });
+
+        // routes.updateStuff()
+        $.ajax({
+            url: '/stuff/'+id,
+            type: 'PUT',
+            data: {
+                "name": "To be deleted"
+            },
+            success: function (data) {
+                $.get('/stuff/'+id, function (data) {
+                    // This will display after the delete.  Welcome to the world of asynch code! :P
+                    $('#aDiv').append('<p>After update:</p><p>' + JSON.stringify(data, undefined, 2) + '</p>');
+                });
+            }
+        });
+
+        // routes.delete()
+        $.ajax({
+            url: '/stuff/'+id,
+            type: 'DELETE',
+            success: function (data) {
+                $('#aDiv').append('<p>Deleted object with id:' + id + '</p>');
+            }
+        });
     });
 });
